e3051471f2bf297d85e2fbee1e3025fb27a4d601
Mrluo <admin@9idk.com>
2016-07-28 16:06:59 +0800

    选择排序

--- ALG/src/sort/SelectionSort.java
+++ ALG/src/sort/SelectionSort.java
@@ -1,5 +1,7 @@
 package sort;
 
+import java.util.Arrays;
+
 /**
  * 选择排序
  * 基本思想：
@@ -11,5 +13,22 @@ package sort;
  *
  */
 public class SelectionSort {
-
+	public static void main(String[] args) {
+		//初始数据
+		int num[]={79,12,3,56,34,28,9,100,88};
+		//中间变量
+		int tem;
+		for(int i=0 ; i<num.length-1 ; i++){
+			int minIndex=i;//最小值得下标
+			for(int j=i+1 ; j<num.length ; j++){
+				if(num[j]<num[minIndex]){
+					minIndex=j;
+				}
+			}
+			tem=num[i];
+			num[i]=num[minIndex];
+			num[minIndex]=tem;
+		}
+		System.out.println(Arrays.toString(num));
+	}
 }
