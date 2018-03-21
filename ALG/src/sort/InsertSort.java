2330299a975e0df23f83bfb68e94aa6d24b596aa
罗云强 <99190092@qq.com>
2018-03-21 22:08:36 +0800

    1

--- ALG/src/sort/InsertSort.java
+++ ALG/src/sort/InsertSort.java
@@ -19,6 +19,15 @@ public class InsertSort {
      * @return
      */
     private static int[] insertSort(int[] init){
+
+        for(int i = 1 ,len = init.length; i< len ; i++){//从第二个元素开始往前插入
+            int temp = init[i]; //记录当前需要插入的数据
+            int j;             //记录当前的位置
+            for(j = i-1 ; j >=0 && temp < init[j] ; j--){ //与前面排好序的数据一个个比较，比前面小就把前面的数据往后移动
+                init[j+1] = init[j];  //移动数据
+            }
+            init[j+1] = temp;  //将之前保存的需要插入的数据插入到移动空出来的位置
+        }
         return init;
     }
 }
