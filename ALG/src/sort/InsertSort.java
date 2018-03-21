5785ad130f60d9edbe3d07a04b4c0e36ca73b6a4
罗云强 <99190092@qq.com>
2018-03-21 21:36:03 +0800

    1

--- ALG/src/sort/InsertSort.java
+++ ALG/src/sort/InsertSort.java
@@ -1,18 +1,24 @@
 package sort;
 
+import java.util.Arrays;
+
 /**
  * 插入排序
  */
 public class InsertSort {
     public static void main(String[] args) {
         int[] init = {12,3434,1,45,978,3,78,9,6,213,4,75,1,3};
-        System.out.println(init);
-        int[] result = insertSort();
-        System.out.println(result);
+        System.out.println(Arrays.toString(init));
+        int[] result = insertSort(init);
+        System.out.println(Arrays.toString(result));
     }
 
-        private static int[] insertSort(){
-        int[] a = {};
-        return a;
+    /**
+     * 排序方法
+     * @param init
+     * @return
+     */
+    private static int[] insertSort(int[] init){
+        return init;
     }
 }
