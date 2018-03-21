2fd0d3da6290acfd7964923e475f0adb64da5d5f
lyq3 <99190092@qq.com>
2018-03-21 17:30:47 +0800

    1

---
+++ ALG/src/sort/InsertSort.java
@@ -0,0 +1,18 @@
+package sort;
+
+/**
+ * 插入排序
+ */
+public class InsertSort {
+    public static void main(String[] args) {
+        int[] init = {12,3434,1,45,978,3,78,9,6,213,4,75,1,3};
+        System.out.println(init);
+        int[] result = insertSort();
+        System.out.println(result);
+    }
+
+        private static int[] insertSort(){
+        int[] a = {};
+        return a;
+    }
+}
